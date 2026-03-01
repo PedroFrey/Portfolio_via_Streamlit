5b3089d2113d2bfad80bd0a4e2e4618b74c04982
Pedro Frey <59786642+PedroFrey@users.noreply.github.com>
2026-03-01 23:31:06 +0000

    Novo yaml

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from Portfolio_via_Streamlit.apps import portfolio#, retirement, stocks, price_comparator, puxa_conversa
+from Portfolio_via_Streamlit.services.notifications_service import send_whatsapp_message
+
+# Chama ao iniciar a app
+send_whatsapp_message("Entraram no Render")
 
 page_names_to_funcs = {
     "My Portfolio": portfolio.portfolio_app,
